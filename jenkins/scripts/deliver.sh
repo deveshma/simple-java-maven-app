b77639e8c2fc296d2fcc6e7c239cf38761523c89
Devesh <deveshma@amazon.com>
2023-09-25 12:37:44 +0000

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -12,20 +12,16 @@ echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
 NAME=`mvn help:evaluate -Dexpression=project.name -q -DforceStdout`
-NAMEFULL=`mvn help:evaluate -Dexpression=project.name`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
-VERSIONFULL=`mvn help:evaluate -Dexpression=project.version`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 echo $NAME
-echo $NAMEFULL
 echo $VERSION
-echo $VERSIONFULL
